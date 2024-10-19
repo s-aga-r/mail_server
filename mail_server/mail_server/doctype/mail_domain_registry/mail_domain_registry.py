1decc5bf17f01504ced79d19f504288ffdd66a13
s-aga-r <sagarsharma.s312@gmail.com>
2024-10-19 18:00:45 +0530

    feat: generate `DKIM Key` on `MailDomainRegistry` creation

--- mail_server/mail_server/doctype/mail_domain_registry/mail_domain_registry.py
+++ mail_server/mail_server/doctype/mail_domain_registry/mail_domain_registry.py
@@ -1,9 +1,59 @@
 # Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import cint
+
+from mail_server.mail_server.doctype.dkim_key.dkim_key import create_dkim_key
 
 
 class MailDomainRegistry(Document):
-	pass
+	def validate(self) -> None:
+		self.validate_domain_name()
+		self.validate_is_verified()
+		self.validate_is_subdomain()
+		self.validate_domain_owner()
+		self.validate_dkim_key_size()
+
+		if self.is_new() or self.has_value_changed("dkim_key_size"):
+			create_dkim_key(self.domain_name, cint(self.dkim_key_size))
+
+	def validate_domain_name(self) -> None:
+		"""Validates Domain Name"""
+
+		if not self.domain_name:
+			frappe.throw(_("Domain Name is mandatory."))
+
+		if self.domain_name.strip().lower() != self.domain_name:
+			frappe.throw(_("Domain Name should be in lowercase."))
+
+	def validate_is_verified(self) -> None:
+		"""Validates Is Verified"""
+
+		if not self.enabled:
+			self.is_verified = 0
+
+	def validate_is_subdomain(self) -> None:
+		"""Validates Is Subdomain"""
+
+		if len(self.domain_name.split(".")) > 2:
+			self.is_subdomain = 1
+
+	def validate_domain_owner(self) -> None:
+		"""Validates Domain Owner"""
+
+		if not self.domain_owner:
+			self.domain_owner = frappe.session.user
+
+	def validate_dkim_key_size(self) -> None:
+		"""Validates DKIM Key Size"""
+
+		if self.dkim_key_size:
+			if cint(self.dkim_key_size) < 1024:
+				frappe.throw(_("DKIM Key Size must be greater than 1024."))
+		else:
+			self.dkim_key_size = frappe.db.get_single_value(
+				"Mail Server Settings", "default_dkim_key_size", cache=True
+			)
