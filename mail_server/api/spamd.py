bc0c46473ed19e215fab35940b6c184e47849f51
s-aga-r <sagarsharma.s312@gmail.com>
2024-11-13 11:03:17 +0530

    fix: log spamd API errors

--- mail_server/api/spamd.py
+++ mail_server/api/spamd.py
@@ -11,31 +11,43 @@ from mail_server.mail_server.doctype.spam_check_log.spam_check_log import create
 def scan(message: str) -> dict:
 	"""Returns the spam score, spamd response and scanning mode of the message"""
 
-	message = get_unescaped_message(message)
-	spam_log = create_spam_check_log(message)
-	return {
-		"spam_score": spam_log.spam_score,
-		"spamd_response": spam_log.spamd_response,
-		"scanning_mode": spam_log.scanning_mode,
-	}
+	try:
+		message = get_unescaped_message(message)
+		spam_log = create_spam_check_log(message)
+		return {
+			"spam_score": spam_log.spam_score,
+			"spamd_response": spam_log.spamd_response,
+			"scanning_mode": spam_log.scanning_mode,
+		}
+	except Exception:
+		frappe.log_error(title=_("Spam Check Failed"), message=frappe.get_traceback())
+		raise
 
 
 @frappe.whitelist(methods=["POST"], allow_guest=True)
 def is_spam(message: str, message_type: Literal["Inbound", "Outbound"] = "Outbound") -> bool:
 	"""Returns True if the message is spam else False"""
 
-	message = get_unescaped_message(message)
-	spam_log = create_spam_check_log(message)
-	return spam_log.is_spam(message_type)
+	try:
+		message = get_unescaped_message(message)
+		spam_log = create_spam_check_log(message)
+		return spam_log.is_spam(message_type)
+	except Exception:
+		frappe.log_error(title=_("Spam Check Failed"), message=frappe.get_traceback())
+		raise
 
 
 @frappe.whitelist(methods=["POST"], allow_guest=True)
 def get_spam_score(message: str) -> float:
 	"""Returns the spam score of the message"""
 
-	message = get_unescaped_message(message)
-	spam_log = create_spam_check_log(message)
-	return spam_log.spam_score
+	try:
+		message = get_unescaped_message(message)
+		spam_log = create_spam_check_log(message)
+		return spam_log.spam_score
+	except Exception:
+		frappe.log_error(title=_("Spam Check Failed"), message=frappe.get_traceback())
+		raise
 
 
 def get_unescaped_message(message: str) -> str:
