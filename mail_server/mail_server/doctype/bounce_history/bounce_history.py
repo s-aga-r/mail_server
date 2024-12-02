e3ca87df7b25cf9f1d78061e151c8c6030f643a1
s-aga-r <sagarsharma.s312@gmail.com>
2024-12-02 14:10:45 +0530

    feat: `Bounce History` DocType

---
+++ mail_server/mail_server/doctype/bounce_history/bounce_history.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.utils import add_days, now
+
+
+class BounceHistory(Document):
+	def validate(self) -> None:
+		if self.has_value_changed("bounce_count"):
+			self.set_last_bounce_at()
+			self.set_blocked_until()
+
+	def set_last_bounce_at(self) -> None:
+		"""Sets the last bounce at to the current time"""
+
+		self.last_bounce_at = now()
+
+	def set_blocked_until(self) -> None:
+		"""Sets the blocked until date based on the bounce count"""
+
+		bounce_count = self.bounce_count
+		block_durations = [1, 7, 30, 36500]  # 1 day, 7 days, 30 days, 100 years
+		block_for_days = block_durations[min(bounce_count - 1, len(block_durations) - 1)]
+		self.blocked_until = add_days(now(), block_for_days)
+
+
+def create_or_update_bounce_history(email: str, bounce_increment: int = 1) -> None:
+	"""Create or update the bounce history for the given email"""
+
+	doc = frappe.get_doc("Bounce History", {"email": email})
+
+	if doc:
+		doc.bounce_count += bounce_increment
+	else:
+		doc = frappe.new_doc("Bounce History")
+		doc.email = email
+		doc.bounce_count = bounce_increment
+
+	doc.save(ignore_permissions=True)
